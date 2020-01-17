54e1fc4530d99610f6c5d8d1f2fe59c5675c0cf4
JRHewitt-CC <36314373+JRHewitt-CC@users.noreply.github.com>
2020-01-17 13:04:39 -0800

    Update sec_security_asn1kc.c

--- src/sec_security_asn1kc.c
+++ src/sec_security_asn1kc.c
@@ -737,6 +737,11 @@ Sec_Result SecAsn1KC_Encode(Sec_Asn1KC *kc, SEC_BYTE *buf, SEC_SIZE buf_len, SEC
     {
         *written = der_len;
     }
+    else if (der_len < 0)
+    {
+        SEC_LOG_ERROR("der_encode_to_buffer failed");
+        return SEC_RESULT_FAILURE;
+    }
     else if (der_len > buf_len)
     {
         SEC_LOG_ERROR("der_encode_to_buffer invalide buffer length, der_len = %d, buf_len = %d", der_len, buf_len);
